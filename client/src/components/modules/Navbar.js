dccda3d34587bb00adc371f2867ab10aba3933e2
Viktoriya Tabunshchyk <vikt@mit.edu>
2021-01-12 14:58:27 -0500

    Added navbar and task

---
+++ client/src/components/modules/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div >
+          Hi this is a navbar lol
+      </div>
+    );
+  }
+}
+
+export default Navbar;
---
+++ client/src/components/modules/Task.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Task extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div >
+          Hi this is a task lol
+      </div>
+    );
+  }
+}
+
+export default Task;
